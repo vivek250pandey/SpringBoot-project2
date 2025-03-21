afcc47b208fdf3a8539193d31099ca5f93ab4ea1
manish5122000 <vivekpandey251288@gmail.com>
2025-03-21 11:28:04 +0530

    adding second projec

---
+++ src/main/java/com/user/user_contact/ProfileEpfFormApplication.java
@@ -0,0 +1,13 @@
+package com.user.user_contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileEpfFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileEpfFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/user_contact/ProfileEpfFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.user_contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileEpfFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
